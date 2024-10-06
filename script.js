e0bbc976750520377b7acd082999dd890ccdafa6
karthikvankayala <koushik.karthik123@gmail.com>
2024-10-06 02:33:40 -0500

    finished code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function toggleMenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
-    
-}
+  }
+  function flipCard() {
+    const container = document.querySelector('.details-container');
+    container.classList.toggle('flip');
+  }
+  
